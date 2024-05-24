f8571e28e30469f57d17726eb353a0cdd34869ee
Julien Couvreur <julien.couvreur@gmail.com>
2024-05-23 20:54:42 -0700

    Update icons

--- toggle-sidebar.js
+++ toggle-sidebar.js
@@ -1,16 +1,18 @@
 
-chrome.contextMenus.create(
-{
-  id: "toggleGitHubSidebar",
-  title: "Toggle GitHub sidebar",
-  contexts: ["page"],
-  "documentUrlPatterns": ["https://github.com/*"]
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create(
+  {
+    id: "toggleGitHubSidebar",
+    title: "Toggle GitHub sidebar",
+    contexts: ["page"],
+    "documentUrlPatterns": ["https://github.com/*/pull/*", "https://github.com/*/issues/*"]
+  });
 });
-  
+
 chrome.contextMenus.onClicked.addListener(function(info, tab) 
 {
   if (info.menuItemId == "toggleGitHubSidebar") 
   {
-    chrome.tabs.sendMessage(tab.id, "toggleGitHubSidebar", {frameId: info.frameId});
+    chrome.tabs.sendMessage(tab.id, "toggleGitHubSidebar", { frameId: info.frameId });
   }
 });
